2d0a1aa89633b99d28a4cbccbba43e8979ebc0ea
AndresDavVF <90978223+AndresDavVF@users.noreply.github.com>
2024-05-16 21:51:18 -0500

    ast10

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -7,25 +7,25 @@ const Menu = () => {
         <ul className='nav nav-pills nav-sidebar flex-column' data-widget='treeview' role='menu' data-accordion='false'>
             <li className='nav-item'>
                 <Link to={"/home"} className='nav-link'>
-                    <i className='nav-icon fa fa-bars'/>
+                    <i className='nav-icon fa fa-play'/>
                     <p>INICIO</p>
                 </Link>
             </li>
             <li className='nav-item'>
                 <Link to={"/clientes"} className='nav-link'>
-                    <i className='nav-icon fa fa-bars'/>
+                    <i className='nav-icon fa fa-user'/>
                     <p>CLIENTES</p>
                 </Link>
             </li>
             <li className='nav-item'>
                 <Link to={"/productos"} className='nav-link'>
-                    <i className='nav-icon fa fa-bars'/>
+                    <i className='nav-icon fa fa-cube'/>
                     <p>PRODUCTOS</p>
                 </Link>
             </li>
             <li className='nav-item'>
                 <Link to={"/proveedores"} className='nav-link'>
-                    <i className='nav-icon fa fa-bars'/>
+                    <i className='nav-icon fa fa-truck'/>
                     <p>PROVEEDORES</p>
                 </Link>
             </li>
